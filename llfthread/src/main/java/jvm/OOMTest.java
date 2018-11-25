94202337b0f514e962a2c95731161103cbf17324
luolf <36410964@qq.com>
2018-11-25 23:35:01 +0800

    静态对象、代码的加载顺序测试复习

---
+++ llfthread/src/main/java/jvm/OOMTest.java
@@ -0,0 +1,21 @@
+package jvm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class OOMTest {
+    public static void main(String[]  sd){
+        List<Object> list=new LinkedList<Object>();
+        int i=0;
+//        for(;;){
+//        while(true){
+//            i++;
+//             if(i%100000==0){
+//                 System.out.println(i);
+//             }
+//            list.add(new Object());
+//        }
+
+    }
+}
